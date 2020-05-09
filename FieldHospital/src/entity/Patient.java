fb34e355a9501119c7df3fdc24837f2344b9cea3
Lucas <lucas.henrique.s.go@gmail.com>
2020-05-09 18:30:57 -0300

    Implementation of Getters and Setters

--- FieldHospital/src/entity/Patient.java
+++ FieldHospital/src/entity/Patient.java
@@ -5,17 +5,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author Lucas
  */
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "Pessoa.findAll", query = "SELECT p FROM Pessoa p")
+})
 public class Patient implements Serializable, BaseEntity {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    private String nome; 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public Long getId() {
         return id;
--- FieldHospital/src/model/PersonDoctorModel.java
+++ FieldHospital/src/model/PersonDoctorModel.java
@@ -1,19 +1,31 @@
+package model; 
+
+import entity.Doctor; 
+import java.awt.List;
+import javax.swing.table.AbstractTableModel;
 
 
+public class PersonDoctorModel extends AbstractTableModel {
+    
+    private String [] columnNames = {"ID_Paciente", "Nome", "Médico", "Ala", "Situação"};
+    private List<Doctor> consultas; 
 
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
-
-/**
- *
- * @author Lucas
- */
-public class PersonDoctorModel {
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
     
 }
