605a6ab4cc3ce26a90f07811cadf5eec5679045c
Lucas <lucas.henrique.s.go@gmail.com>
2020-05-10 11:30:47 -0300

    Classes From DataBase
    Class added from the database

---
+++ FieldHospital/src/entity/Medicos.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Lucas
+ */
+@Entity
+@Table(name = "medicos")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Medicos.findAll", query = "SELECT m FROM Medicos m"),
+    @NamedQuery(name = "Medicos.findById", query = "SELECT m FROM Medicos m WHERE m.id = :id"),
+    @NamedQuery(name = "Medicos.findByNome", query = "SELECT m FROM Medicos m WHERE m.nome = :nome"),
+    @NamedQuery(name = "Medicos.findByCrm", query = "SELECT m FROM Medicos m WHERE m.crm = :crm"),
+    @NamedQuery(name = "Medicos.findByCpf", query = "SELECT m FROM Medicos m WHERE m.cpf = :cpf")})
+public class Medicos implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+    @Basic(optional = false)
+    @Column(name = "crm")
+    private String crm;
+    @Basic(optional = false)
+    @Column(name = "cpf")
+    private String cpf;
+
+    public Medicos() {
+    }
+
+    public Medicos(Integer id) {
+        this.id = id;
+    }
+
+    public Medicos(Integer id, String nome, String crm, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.crm = crm;
+        this.cpf = cpf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Medicos)) {
+            return false;
+        }
+        Medicos other = (Medicos) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Medicos[ id=" + id + " ]";
+    }
+    
+}
